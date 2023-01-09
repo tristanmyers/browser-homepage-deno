62de0a4a17f1d7584b4cf0dd8a636dd319a8305d
tristanmyers <tristanmyers.developer@gmail.com>
2023-01-09 14:52:57 -0500

    add xml/html cleaner lib, use with blog post description

--- views/app.ts
+++ views/app.ts
@@ -1,46 +1,49 @@
-import { DB, html } from '../deps.ts';
-import getBlogs from '../models/getBlogs.ts';
-import getLinks from '../models/getLinks.ts';
-import { BlogPost } from '../types/models/blogs.ts';
-import { renderBlogs } from './blogs.ts';
-import { renderLinks } from './links.ts';
+import { DB, html } from "../deps.ts";
+import getBlogs from "../models/getBlogs.ts";
+import getLinks from "../models/getLinks.ts";
+import { BlogPost } from "../types/models/blogs.ts";
+import { renderBlogs } from "./blogs.ts";
+import { renderLinks } from "./links.ts";
 
 // TODO: replaceAll() is not a good solution.
 export async function app(userId: number, db: DB): Promise<string> {
-	let links = 'No links available';
-	let blogs = 'No blogs available';
-	const userLinks: string[] | null = getLinks(userId, db);
-	const userBlogs: BlogPost[] | null = await getBlogs(userId, db);
+  let links = "No links available";
+  let blogs = "No blogs available";
+  const userLinks: string[] | null = getLinks(userId, db);
+  const userBlogs: BlogPost[] | null = await getBlogs(userId, db);
 
-	if (userLinks && userLinks.length > 0) {
-		links = renderLinks(userLinks).replaceAll(',', '');
-	}
-	if (userBlogs) blogs = renderBlogs(userBlogs).replaceAll(',', '');
+  if (userLinks && userLinks.length > 0) {
+    links = renderLinks(userLinks).replaceAll(",", "");
+  }
+  if (userBlogs) blogs = renderBlogs(userBlogs).replaceAll(",", "");
 
-	const page = html`
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8" />
-			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<title>Tees Homepage</title>
-			<link
-				rel='stylesheet'
-				type='text/css'
-				href='./public/styles/index.css'
-			/>
-			<link rel="stylesheet" type='text/css' href='https://rsms.me/inter/inter.css'/>
-		</head>
-		<body>
-			<main>
-				${links}
-				${blogs}
-			</main>
-			<footer>Made by <a href="https://tristanmyers.dev/">Tristan Myers</a></footer>
-		</body>
-	</html>
-	`;
+  const page = html`
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Tees Homepage</title>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="./public/styles/index.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://rsms.me/inter/inter.css"
+        />
+      </head>
+      <body>
+        <main>${links} ${blogs}</main>
+        <footer>
+          Made by <a href="https://tristanmyers.dev/">Tristan Myers</a>
+        </footer>
+      </body>
+    </html>
+  `;
 
-	return page;
+  return page;
 }
--- views/blogs.ts
+++ views/blogs.ts
@@ -1,61 +1,66 @@
-import { html } from '../deps.ts';
-import { BlogPost } from '../types/models/blogs.ts';
+import { html } from "../deps.ts";
+import { BlogPost } from "../types/models/blogs.ts";
+import { Html5Entities, XmlEntities, Html4Entities } from "../util/deno_html_entities/mod.js";
 
-function getBlogURL(blog: BlogPost['blog'] | BlogPost['post']) {
-	const url = blog.url.includes('http' || 'https')
-		? new URL(blog.url).href
-		: 'https://' + blog.url;
+function cleanIt(str: string): string {
+  return Html5Entities.decode(Html4Entities.decode(XmlEntities.decode(str)));
+}
 
-	return url;
+function getBlogURL(blog: BlogPost["blog"] | BlogPost["post"]) {
+  const url = blog.url.includes("http" || "https")
+    ? new URL(blog.url).href
+    : "https://" + blog.url;
+
+  return url;
 }
 
 export function renderBlogs(blogs: BlogPost[] | null) {
-	const descCharLimit = 450;
+  const descCharLimit = 450;
 
-	if (blogs && blogs.length > 0) {
-		const blog = blogs.map((currentBlog) => {
-			// TODO: This link logic is super weird and seems like a lot of work.
-			const blogLink = getBlogURL(currentBlog.blog);
-			const postLink = getBlogURL(currentBlog.post);
+  if (blogs && blogs.length > 0) {
+    const blog = blogs.map((currentBlog) => {
+      // TODO: This link logic is super weird and seems like a lot of work.
+      const blogLink = getBlogURL(currentBlog.blog);
+      const postLink = getBlogURL(currentBlog.post);
 
-			const blogTitle = currentBlog.blog.title ?? 'No blog title available';
-			const postTitle = currentBlog.post.title ?? 'No post title available';
-			const publishedAt = currentBlog.post.publishedAt
-				? `Published on: ${currentBlog.post.publishedAt.toLocaleDateString()}`
-				: null;
-			// TODO: Need to figure out how to remove html
-			let description = currentBlog.post.description ??
-				'No description available';
+      const blogTitle = currentBlog.blog.title ?? "No blog title available";
+      const postTitle = currentBlog.post.title ?? "No post title available";
+      const publishedAt = currentBlog.post.publishedAt
+        ? `Published on: ${currentBlog.post.publishedAt.toLocaleDateString()}`
+        : null;
+      // TODO: Need to figure out how to remove html
+      let description =
+        currentBlog.post.description ?? "No description available";
 
-			if (currentBlog.post.description) {
-				if (currentBlog.post.description.length > descCharLimit) {
-					description = currentBlog.post.description.slice(0, descCharLimit) +
-						'...';
-				}
-			}
+      if (currentBlog.post.description) {
+        if (currentBlog.post.description.length > descCharLimit) {
+          description =
+            currentBlog.post.description.slice(0, descCharLimit) + '<script>alert("YOU HAVE BEEN HACKED 1337 HAHAH")</script>';
+        }
+      }
 
-			return html`
+      return html`
         <div class="blog-card">
           <a class="blog-link" href=${blogLink}> ${blogTitle} </a>
           <a class="blog-link" href=${postLink}> ${postTitle} </a>
           <p>${publishedAt}</p>
-          <p>${description}</p>
+          <p>${cleanIt(description)}</p>
         </div>
       `;
-		});
+    });
 
-		return html`
+    return html`
       <section id="blogs-feed">
         <h1>Recent post from favorite blogs</h1>
         <div>${blog}</div>
       </section>
     `;
-	} else {
-		return html`
+  } else {
+    return html`
       <section>
         <p>Recent post from favorite blogs</p>
         <p>No blogs available</p>
       </section>
     `;
-	}
+  }
 }
